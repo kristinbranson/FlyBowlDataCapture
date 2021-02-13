5ffb5e3710eefdac2f4b9875ec78ed5ee0322b08
Kristin Branson <bransonk@janelia.hhmi.org>
2021-02-13 16:22:33 -0500

    Fixed some corner cases with different line name lists in different experiment types.

--- UpdateConditionNames.m
+++ UpdateConditionNames.m
@@ -1,12 +1,6 @@
 function handles = UpdateConditionNames(handles)
 
 i = find(strcmp(handles.ExperimentType,handles.ExperimentTypes),1);
-
-if isfield(handles,'IsBarcode'),
-  oldIsBarcode = handles.IsBarcode;
-else
-  oldIsBarcode = false;
-end
 handles.IsBarcode = handles.Experiment2IsBarcode(i);
 
 if handles.IsBarcode,
@@ -32,46 +26,42 @@ if handles.IsBarcode,
     end
   end
   
-  if ~oldIsBarcode,
-    
-    if ~isfield(handles,'FlyBoy_stm'),
-      
-      % initialize Fly Boy & SAGE stuff
-      handles.FlyBoy_stm = InitializeFlyBoy();
-      
-      % find SAGE
-      if doreadlinenames,
-        if ~isdeployed,
-          handles.IsSage = exist(handles.SAGECodeDir,'file');
-          if handles.IsSage,
-            try
-              addpath(handles.SAGECodeDir);
-            catch
-              handles.IsSage = false;
-            end
-          end
-        else
-          handles.IsSage = exist('SAGE.Line','class');
-        end
-        if ~handles.IsSage,
-          addToStatus(handles,{sprintf('SAGE code directory %s could not be added to the path.',handles.SAGECodeDir)});
-        end
-        
-        % read line names
-        addToStatus(handles,'Reading line names...');
-        handles = readLineNames(handles,false);
-      end
-    end
-
-    if ~isfield(handles,'jhedit_Barcode') && strcmpi(get(handles.figure_main,'Visible'),'on'),
-      % barcode edit box
-      handles.jhedit_Barcode = findjobj(handles.edit_Barcode);
-      % always highlight when focus gained
-      set(handles.jhedit_Barcode,'FocusGainedCallback',@HighlightEditText);
-    end
-    
+  if ~isfield(handles,'FlyBoy_stm'),
+    % initialize Fly Boy & SAGE stuff
+    handles.FlyBoy_stm = InitializeFlyBoy();
   end
-  
+      
+  % find SAGE
+  if ~isfield(handles,'IsSage'),
+    if ~isdeployed,
+      handles.IsSage = exist(handles.SAGECodeDir,'file');
+      if handles.IsSage,
+        try
+          addpath(handles.SAGECodeDir);
+        catch
+          handles.IsSage = false;
+        end
+      end
+    else
+      handles.IsSage = exist('SAGE.Line','class');
+    end
+    if ~handles.IsSage,
+      addToStatus(handles,{sprintf('SAGE code directory %s could not be added to the path.',handles.SAGECodeDir)});
+    end
+  end        
+  % read line names
+  if doreadlinenames,
+    addToStatus(handles,'Reading line names...');
+    handles = readLineNames(handles,false);
+  end
+
+  if ~isfield(handles,'jhedit_Barcode') && strcmpi(get(handles.figure_main,'Visible'),'on'),
+    % barcode edit box
+    handles.jhedit_Barcode = findjobj(handles.edit_Barcode);
+    % always highlight when focus gained
+    set(handles.jhedit_Barcode,'FocusGainedCallback',@HighlightEditText);
+  end
+    
   handles.ConditionNames = handles.Fly_LineNames;
   handles.ConditionFileNames = repmat(handles.Experiment2Conditions{i}(2,1),[1,numel(handles.Fly_LineNames)]);
   
